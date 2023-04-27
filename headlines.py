b35f4d003d77939121001ab21a54df06063d9d7e
Jaquior-qiao <Jaquior-qiao@github.com>
2023-04-27 16:39:54 +0800

    achieve the static url to get rss

--- headlines.py
+++ headlines.py
@@ -3,23 +3,38 @@ import feedparser
 
 app = Flask(__name__)
 
-RSS_FEED = "https://www.ruanyifeng.com/blog/atom.xml"
+RSS_FEED_DICT = {
+    "ryf": "https://www.ruanyifeng.com/blog/atom.xml",
+    "fox": "https://feeds.foxnews.com/foxnews/latest",
+    "iol": "https://www.iol.co.za/cmlink/1.640",
+}
+
 
 @app.route("/")
-def get_news():
-    feed = feedparser.parse(RSS_FEED)
-    # print(feed)
+@app.route("/ryf")
+def ryf():
+    return get_news("ryf")
+
+
+@app.route("/fox")
+def fox():
+    return get_news("fox")
+
+
+def get_news(rss):
+    feed = feedparser.parse(RSS_FEED_DICT.get(rss))
     first_article = feed["entries"][0]
     return """<html>
         <body>
-            <h1> 阮一峰的网络日志 </h1>
+            <h1> {3} </h1>
             <b>{0}</b> <br/>
             <i>{1}</i> <br/>
             <p>{2}</p> <br/>
             </body>
         </html>""".format(first_article.get("title"),
                           first_article.get("published"),
-                          first_article.get("summary"))
+                          first_article.get("summary"),
+                          feed["feed"].get("title"))
 
 
 if __name__ == "__main__":
